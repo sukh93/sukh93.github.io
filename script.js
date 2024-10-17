77ed7efed8bd8281cbe1d41a72efbc19f3e11033
sukh93 <sukh93@yahoo.com>
2024-10-17 00:45:14 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Scroll animation (optional)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
